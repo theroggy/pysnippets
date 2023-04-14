9c6557f64baf0a05ab8243e39827f29a0ae03e6a
Pieter Roggemans <pieter.roggemans@gmail.com>
2023-04-15 00:08:51 +0200

    Add dissolve_intersecting script

---
+++ pysnippets/stackoverflow_questions/dissolve_intersecting.py
@@ -0,0 +1,31 @@
+import json
+from pathlib import Path
+
+import geopandas as gpd
+import matplotlib.pyplot as plt
+import pandas as pd
+
+script_dir = Path(__file__).resolve().parent
+with open(script_dir / "dissolve_intersecting.geojson", "r") as f:
+    data = json.load(f)
+
+gdf = gpd.GeoDataFrame.from_features(data["features"])
+
+# Calculate intersections within the layer
+intersection_gdf = gdf.overlay(gdf, how="intersection", keep_geom_type=True)
+intersection_gdf = intersection_gdf.loc[
+    intersection_gdf.name_1 != intersection_gdf.name_2
+]
+
+# The features to dissolve are the intersecting one, excluding the self-intersections
+to_dissolve_gdf = gdf.loc[
+    gdf.name.isin(intersection_gdf.name_1) | gdf.name.isin(intersection_gdf.name_2)
+]
+
+# Other features should not be dissolved
+no_dissolve_gdf = gdf.loc[~gdf.index.isin(to_dissolve_gdf.index)]
+
+# Compile + plot the result
+result_gdf = pd.concat([to_dissolve_gdf.dissolve(), no_dissolve_gdf])
+result_gdf.plot(categorical=True, cmap="Set1", alpha=0.66)
+plt.show()
