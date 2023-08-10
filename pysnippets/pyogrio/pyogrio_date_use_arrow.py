4f4477bba0b4a3799de8ff8f84ed7f11d33a6cad
Pieter Roggemans <pieter.roggemans@gmail.com>
2023-08-10 16:48:53 +0200

    Update pyogrio_date_use_arrow.py

--- pysnippets/pyogrio/pyogrio_date_use_arrow.py
+++ pysnippets/pyogrio/pyogrio_date_use_arrow.py
@@ -1,10 +1,20 @@
+from pathlib import Path
+import tempfile
+
 import pyogrio
 
-url = "https://github.com/theroggy/pysnippets/raw/pysnippets/pyogrio/polygon-parcel_31370.zip"
-
+tmp_dir = Path(tempfile.gettempdir())
+url = "https://github.com/theroggy/pysnippets/raw/main/pysnippets/pyogrio/polygon-parcel_31370.zip"
 for use_arrow in [True, False]:
     gdf = pyogrio.read_dataframe(url, use_arrow=use_arrow)
     print(
-        f"use_arrow: {use_arrow}, column dtype: {gdf['DATUM'].dtype}, "
+        f"input_file, use_arrow: {use_arrow}, column dtype: {gdf['DATUM'].dtype}, "
+        f"value: {gdf['DATUM'][0]}, value type: {type(gdf['DATUM'][0])}"
+    )
+    written_path = tmp_dir / f"output_{use_arrow}.shp"
+    pyogrio.write_dataframe(gdf, written_path)
+    gdf = pyogrio.read_dataframe(written_path, use_arrow=False)
+    print(
+        f"written_file, use_arrow: {use_arrow}, column dtype: {gdf['DATUM'].dtype}, "
         f"value: {gdf['DATUM'][0]}, value type: {type(gdf['DATUM'][0])}"
     )
