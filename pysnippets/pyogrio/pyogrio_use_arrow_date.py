9f92c5df45659253c1c244152102e84500b6065c
Pieter Roggemans <pieter.roggemans@gmail.com>
2023-11-06 07:31:24 +0100

    Add fiona roundtrip for comparison

--- pysnippets/pyogrio/pyogrio_use_arrow_date.py
+++ pysnippets/pyogrio/pyogrio_use_arrow_date.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import tempfile
 
+import geopandas as gpd
 import pyogrio
 
 tmp_dir = Path(tempfile.gettempdir())
@@ -18,3 +19,17 @@ for use_arrow in [True, False]:
         f"written_file, use_arrow: {use_arrow}, column dtype: {gdf['DATUM'].dtype}, "
         f"value: {gdf['DATUM'][0]}, value type: {type(gdf['DATUM'][0])}"
     )
+
+# Do a round-trip using fiona for comparison
+gdf = gpd.read_file(url, engine="fiona")
+print(
+    f"input_file, fiona, column dtype: {gdf['DATUM'].dtype}, "
+    f"value: {gdf['DATUM'][0]}, value type: {type(gdf['DATUM'][0])}"
+)
+written_path = tmp_dir / "output_fiona.shp"
+gdf.to_file(written_path)
+gdf = gpd.read_file(written_path, engine="fiona")
+print(
+    f"written_file, fiona, column dtype: {gdf['DATUM'].dtype}, "
+    f"value: {gdf['DATUM'][0]}, value type: {type(gdf['DATUM'][0])}"
+)
