796ebc936d930d3f4b3322d302d1164d10f607e1
Pieter Roggemans <pieter.roggemans@gmail.com>
2024-01-08 18:21:15 +0100

    Create overlapping_collection_intersects.py

---
+++ pysnippets/libgeos/overlapping_collection_intersects.py
@@ -0,0 +1,20 @@
+import shapely
+
+# Difference of geom: 1 polygon, geom: GeometryCollection of 2 overlapping polygons
+# Result: TopologyException
+geom = shapely.Polygon([(0, 0), (50, 0), (50, 50), (0, 50), (0, 0)])
+geom_collection = shapely.GeometryCollection(
+    [
+        shapely.Polygon([(0, 0), (10, 0), (10, 10), (0, 10), (0, 0)]),
+        shapely.Polygon([(5, 0), (20, 0), (20, 10), (5, 10), (5, 0)]),
+    ]
+)
+print(f"versions: shapely: {shapely.__version__}, geos-C-api: {shapely.geos_capi_version_string}")
+
+print(f"is simple poly valid? {shapely.is_valid_reason(geom)}")
+print(f"is collection valid? {shapely.is_valid_reason(geom_collection)}")
+
+try:
+    print(f"geom.intersects(geom_collection): {geom.intersects(geom_collection)}")
+except Exception as ex:
+    print(f"Exception raised: {ex}")
