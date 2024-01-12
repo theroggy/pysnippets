b90b82b9e644f88314f052e39a3c0d541df85a1c
Pieter Roggemans <pieter.roggemans@gmail.com>
2024-01-12 09:41:33 +0100

    Create pointZ_to_point.py

---
+++ pysnippets/stackoverflow_questions/pointZ_to_point.py
@@ -0,0 +1,8 @@
+    import geopandas
+    from shapely.geometry import Point
+
+    series_z = geopandas.GeoSeries([Point(1, 1, 1), Point(2, 2, 2), Point(3, 3, 3)])
+    print(series_z)
+
+    series_xy = geopandas.GeoSeries(geopandas.points_from_xy(x=series_z.x, y=series_z.y))
+    print(series_xy)
