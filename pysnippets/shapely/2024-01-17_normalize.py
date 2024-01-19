47eaf1b7f9383221edc9013600cb5a5b1122ab72
Pieter Roggemans <pieter.roggemans@gmail.com>
2024-01-19 23:29:24 +0100

    Add tests on geometrycollections

--- pysnippets/shapely/2024-01-17_normalize.py
+++ pysnippets/shapely/2024-01-17_normalize.py
@@ -1,6 +1,7 @@
 import shapely
 
-p1_orig = shapely.geometry.Polygon([(0, 100), (0, 0), (100, 0), (100, 100)])
+# Tests if input is counter clockwise
+p1_orig = shapely.Polygon([(0, 100), (0, 0), (100, 0), (100, 100)])
 p1_set_prec = shapely.set_precision(p1_orig, grid_size=1e-3)
 p1_orig_norm = shapely.normalize(p1_orig)
 p1_set_prec_norm = shapely.normalize(p1_set_prec)
@@ -16,8 +17,19 @@ print(f"{p1_inters_orig=} (clockwise)")
 print(f"{p1_inters_set_prec=} (clockwise)")
 print(f"{p1_inters_orig_norm=} (clockwise)")
 
-p2_orig = shapely.geometry.Polygon([(0, 100), (100, 100), (100, 0), (0, 0)])
+# Tests if input is clockwise
+p2_orig = shapely.Polygon([(0, 100), (100, 100), (100, 0), (0, 0)])
 p2_inters_orig = shapely.intersection(p2_orig, p2_orig)
 
 print(f"{p2_orig=} (clockwise)")
 print(f"{p2_inters_orig=} (clockwise)")
+
+# Tests on ordering of geometrycollections
+l1_orig = shapely.LineString([(200, 100), (200, 0), (300, 0), (300, 100)])
+c1_orig = shapely.GeometryCollection([p1_orig, l1_orig])
+c1_inters = c1_orig.intersection(c1_orig.envelope)
+print(f"c1_inters={c1_inters.wkt}")
+c1_inters_norm = c1_inters.normalize()
+print(f"c1_inters_norm={c1_inters_norm.wkt}")
+c1_norm = c1_orig.normalize()
+print(f"c1_norm={c1_norm.wkt}")
