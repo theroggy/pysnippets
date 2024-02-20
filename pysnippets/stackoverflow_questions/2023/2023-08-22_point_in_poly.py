b554c7d3d218df2782dee1e436327549b523eaea
Pieter Roggemans <pieter.roggemans@gmail.com>
2024-02-20 08:51:34 +0100

    Create 2023-08-22_point_in_poly.py

---
+++ pysnippets/stackoverflow_questions/2023/2023-08-22_point_in_poly.py
@@ -0,0 +1,12 @@
+    import geopandas as gpd
+    import shapely
+
+    polys = [
+        shapely.box(xmin=xmin, ymin=ymin, xmax=xmin + 10, ymax=ymin + 10)
+        for xmin in range(0, 100, 10)
+        for ymin in range(0, 100, 10)
+    ]
+    grid = gpd.GeoDataFrame(geometry=polys)
+
+    # Search for polygons that intersect with the point
+    print(grid.iloc[grid.sindex.query(shapely.Point(55, 55), predicate="intersects")])
