8f9777696747570fa61c0cab04f1d03083425758
Pieter Roggemans <pieter.roggemans@gmail.com>
2024-03-09 19:20:16 +0100

    Create to_file_attribute_table.py

---
+++ pysnippets/geopandas/to_file_attribute_table.py
@@ -0,0 +1,18 @@
+import geopandas as gpd
+
+df = gpd.read_file(r"C:\Temp\polygon-parcel.gpkg", ignore_geometry=True)
+gdf = gpd.GeoDataFrame(df)
+
+gdf.to_file("/vsimem/memoryfile_pyogrio.gpkg", engine="pyogrio")
+result_pyogrio_gdf = gpd.read_file("/vsimem/memoryfile_pyogrio.gpkg")
+
+gdf.to_file("/vsimem/memoryfile_fiona.gpkg", engine="fiona")
+result_fiona_gdf = gpd.read_file("/vsimem/memoryfile_fiona.gpkg")
+
+gdf2 = gpd.GeoDataFrame({"data_column": [1, 2, 3]})
+gdf2.to_file("/vsimem/memoryfile_pyogrio2.gpkg", engine="pyogrio")
+result_pyogrio2_gdf = gpd.read_file("/vsimem/memoryfile_pyogrio2.gpkg")
+
+print(result_pyogrio_gdf)
+print(result_fiona_gdf)
+print(result_pyogrio2_gdf)
