b3cad94fcb8073d2421d4e35f79a033b11b34d1d
Pieter Roggemans <pieter.roggemans@gmail.com>
2024-03-09 19:18:45 +0100

    Cleanup printing

--- pysnippets/shapely/make_valid_keep_collapsed.py
+++ pysnippets/shapely/make_valid_keep_collapsed.py
@@ -1,7 +1,7 @@
 import shapely
 
 poly = shapely.from_wkt("MultiPolygon (((57479 202281, 57479 202259, 57454 202260, 57453 202277, 57479 202281),(57471 202270, 57471 202270, 57467 202270, 57471 202270)))")
-print(f"poly.is_valid: {poly.is_valid}")
+print(f"{poly.is_valid=}")
 poly_valid = shapely.make_valid(poly)
-print(f"poly_valid.is_valid: {poly_valid.is_valid}")
-print(f"poly_valid: {poly_valid.wkt}")
+print(f"{poly_valid.is_valid=}")
+print(f"{poly_valid.wkt=}")
