91b05a735e376a0caf9df7b9f079e8af90f16a60
Pieter Roggemans <pieter.roggemans@gmail.com>
2024-04-08 18:43:21 +0200

    Add test with pyogrio to reading baltic encoding

--- pysnippets/pyogrio/2024-04-08_encoding_baltic/read.py
+++ pysnippets/pyogrio/2024-04-08_encoding_baltic/read.py
@@ -2,5 +2,8 @@ from pathlib import Path
 import geopandas as gpd
 
 path = Path(__file__).resolve().parent / "R05_USER.DBF"
-df = gpd.read_file(path, encoding="cp1257")
+df = gpd.read_file(path, encoding="cp1257", engine="fiona")
+print(df)
+
+df = gpd.read_file(path, encoding="cp1257", engine="pyogrio")
 print(df)
