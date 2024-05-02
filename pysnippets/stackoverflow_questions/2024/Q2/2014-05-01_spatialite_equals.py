71d0f430bbd18a03d724f9077651ee8856f665e1
Pieter Roggemans <pieter.roggemans@gmail.com>
2024-05-02 08:30:58 +0200

    Create 2014-05-01_spatialite_equals.py

---
+++ pysnippets/stackoverflow_questions/2024/Q2/2014-05-01_spatialite_equals.py
@@ -0,0 +1,16 @@
+import geopandas as gpd
+
+path = "C:/temp/lds-nz-building-outlines/nz-building-outlines.gpkg"
+
+wkt1 = "POLYGON((0 5, 5 5, 5 0, 0 0, 0 5))"
+wkt2 = "POLYGON((0 0, 0 5, 5 5, 5 0, 0 0))"
+wkt3 = "POLYGON((0 0, 0 5, 4 5, 5 5, 5 0, 0 0))"
+sql = f"""
+    SELECT GeomFromText('{wkt1}') = GeomFromText('{wkt1}') AS same_operator
+        ,ST_Equals(GeomFromText('{wkt1}'), GeomFromText('{wkt1}')) AS same_st_equals
+        ,GeomFromText('{wkt1}') = GeomFromText('{wkt2}') AS pointorder_operator
+        ,ST_Equals(GeomFromText('{wkt1}'), GeomFromText('{wkt2}')) AS pointorder_st_equals
+        ,GeomFromText('{wkt1}') = GeomFromText('{wkt3}') AS extrapoint_operator
+        ,ST_Equals(GeomFromText('{wkt1}'), GeomFromText('{wkt3}')) AS extrapoint_st_equals
+"""  # noqa: E501
+print(gpd.read_file(path, sql=sql, engine="pyogrio").transpose())
