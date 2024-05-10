58a00e0230bd9a419afe2bb8bf2315bf855ef101
Pieter Roggemans <pieter.roggemans@gmail.com>
2024-05-10 13:45:11 +0200

    Update dissolve.py

--- pysnippets/ogr2ogr/dissolve.py
+++ pysnippets/ogr2ogr/dissolve.py
@@ -6,8 +6,8 @@ https://gis.stackexchange.com/questions/481099/error-in-dissolving-shapefile-wit
 import os
 
 os.system(
-    r'ogr2ogr -dialect sqlite -sql "SELECT ST_Union(geometry) AS geometry FROM ""93000100_qk21_new""" "C:\Temp\polygon_parcel\93000100_qk21_new_1.shp" "C:\Temp\polygon_parcel\93000100_qk21_new.shp"'
+    r'ogr2ogr -dialect sqlite -sql "SELECT ST_Union(geometry) AS geometry FROM \"93000100_qk21_new\"" "C:\Temp\polygon_parcel\93000100_qk21_new_1.shp" "C:\Temp\polygon_parcel\93000100_qk21_new.shp"'
 )
 
 # Command standalone:
-# ogr2ogr -dialect sqlite -sql "SELECT ST_Union(geometry) AS geometry FROM ""93000100_qk21_new""" 93000100_qk21_new_1.shp 93000100_qk21_new.shp
+# ogr2ogr -dialect sqlite -sql "SELECT ST_Union(geometry) AS geometry FROM \"93000100_qk21_new\"" 93000100_qk21_new_1.shp 93000100_qk21_new.shp
