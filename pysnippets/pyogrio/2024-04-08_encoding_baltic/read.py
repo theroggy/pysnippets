01fd38d7a74a2a212792452c7abdd10eecddebe4
Pieter Roggemans <pieter.roggemans@gmail.com>
2024-06-07 17:12:06 +0200

    Small improvement

--- pysnippets/pyogrio/2024-04-08_encoding_baltic/read.py
+++ pysnippets/pyogrio/2024-04-08_encoding_baltic/read.py
@@ -2,8 +2,9 @@ from pathlib import Path
 import geopandas as gpd
 
 path = Path(__file__).resolve().parent / "R05_USER.DBF"
-df = gpd.read_file(path, encoding="cp1257", engine="fiona")
-print(df)
+df = gpd.read_file(path, engine="fiona")
+print(df.transpose())
 
-df = gpd.read_file(path, encoding="cp1257", engine="pyogrio")
-print(df)
+# encoding="cp1257",
+df = gpd.read_file(path, engine="pyogrio")
+print(df.transpose())
