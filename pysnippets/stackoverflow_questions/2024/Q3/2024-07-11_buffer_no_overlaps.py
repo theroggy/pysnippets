165d3ab0ac1e284ebd5df87aef7aa75d0725faa1
Pieter Roggemans <pieter.roggemans@gmail.com>
2024-07-11 20:55:10 +0200

    Create buffer_no_overlaps

---
+++ pysnippets/stackoverflow_questions/2024/Q3/2024-07-11_buffer_no_overlaps.py
@@ -0,0 +1,38 @@
+# https://gis.stackexchange.com/questions/483678/buffer-without-overlaps-in-python
+
+from pathlib import Path
+from matplotlib import pyplot as plt
+import numpy as np
+import pandas as pd
+import geopandas as gpd
+
+# load the csv file and convert to GeoDataFrame
+points_path = Path(__file__).parent / "2024-07-11_buffer_no_overlaps_points_demo.csv"
+df = pd.read_csv(points_path)
+geometry = gpd.points_from_xy(df["Longitude"], df["Latitude"])
+points = gpd.GeoDataFrame(df, geometry=geometry, crs="EPSG:4326")
+
+# change to projected CRS
+points = points.to_crs("EPSG:7856")
+
+# create unique ID for points
+points["point_id"] = np.arange(1, points.shape[0] + 1)
+
+# create voronoi polygons
+voronois = points.copy()
+voronois.geometry = points.geometry.voronoi_polygons()
+
+# specify buffer in meters
+buffer = 10
+
+# create a limit for voronois (union all points and buffer them)
+limit = points.union_all().buffer(buffer)
+
+# clip the voronois to the limit
+result = voronois.clip(limit)
+print(result)
+
+# plot
+ax = result.plot()
+points.plot(ax=ax, color="red")
+plt.show()
