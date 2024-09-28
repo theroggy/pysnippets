1ef1a77857bae412a431955e88cc0827443b5ecf
Pieter Roggemans <pieter.roggemans@gmail.com>
2024-09-28 04:16:59 +0200

    Update 2024-04-04_glob_recursiv.py

--- pysnippets/stackoverflow_questions/2024/Q2/2024-04-04_glob_recursiv.py
+++ pysnippets/stackoverflow_questions/2024/Q2/2024-04-04_glob_recursiv.py
@@ -1,9 +1,9 @@
-    from pathlib import Path
-    import shutil
+from pathlib import Path
+import shutil
 
-    src_dir = Path("C:/temp/test_recursivecopy/src")
-    dst_dir = Path("C:/temp/test_recursivecopy/dst")
+src_dir = Path("C:/temp/test_recursivecopy/src")
+dst_dir = Path("C:/temp/test_recursivecopy/dst")
 
-    files = src_dir.glob("**/*.geojson")
-    for file in files:
-        shutil.copy(file, dst_dir)
+files = src_dir.glob("**/*.geojson")
+for file in files:
+    shutil.copy(file, dst_dir)
