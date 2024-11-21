262e81984a57c8027dd4af71d0bac93995324f8b
Pieter Roggemans <pieter.roggemans@gmail.com>
2024-11-21 23:12:47 +0100

    Update 2024-11-04_filter_pandas.py

--- pysnippets/stackoverflow_questions/2024/Q4/2024-11-04_filter_pandas.py
+++ pysnippets/stackoverflow_questions/2024/Q4/2024-11-04_filter_pandas.py
@@ -5,7 +5,9 @@ https://stackoverflow.com/questions/79154874/combine-different-shapefile-feature
 import geopandas as gpd
 from shapely import box
 
-# vec_data = gpd.read_file("map.shp")
+vec_data = gpd.read_file(r"C:\Temp\LAFIS\LAFIS_update.shp")
+"""
+# Test data if the shapefile is not available
 vec_data = gpd.GeoDataFrame(
     data={"DESCR_ENG": [
         "Meadow (Half Sheed Tara 20%)",
@@ -16,14 +18,18 @@ vec_data = gpd.GeoDataFrame(
     geometry=[box(0, 0, 5, 5), box(5, 0, 10, 5), box(10, 0, 15, 5), box(15, 0, 20, 5)],
     crs=31370,
 )
+"""
 
 # Add new "Classes" column based on DESCR_ENG column
 vec_data["Classes"] = None
 vec_data.loc[vec_data["DESCR_ENG"].str.startswith("Meadow"), "Classes"] = "Agriculture"
 vec_data.loc[vec_data["DESCR_ENG"].str.startswith("Pasture"), "Classes"] = "Cultivated"
 
+vec_data_dissolved = vec_data.dissolve(by="Classes")
+
 # Print result
 print(vec_data.head())
+print(vec_data_dissolved.head())
 
 # Save to file
-# vec_data.to_file("map_extended.shp")
+#vec_data.to_file(r"C:\Temp\LAFIS\LAFIS_update_dissolved.shp")
