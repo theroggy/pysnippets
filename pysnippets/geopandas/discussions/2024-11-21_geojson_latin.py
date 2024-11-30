96d300df31e6ab9fb1f6b48eefa1ecc4d4bab4ae
Pieter Roggemans <pieter.roggemans@gmail.com>
2024-11-30 14:09:18 +0100

    Update 2024-11-21_geojson_latin.py

--- pysnippets/geopandas/discussions/2024-11-21_geojson_latin.py
+++ pysnippets/geopandas/discussions/2024-11-21_geojson_latin.py
@@ -2,8 +2,7 @@ from pathlib import Path
 import geopandas as gpd
 
 path = Path(__file__).with_suffix(".geojson")
-with open(path, 'r', encoding='latin') as f:
-    data = f.read()
-    gdf = gpd.read_file(data)
+with open(path, 'r', encoding='latin') as file:
+    gdf = gpd.read_file(file)
 
 print(gdf.columns)
