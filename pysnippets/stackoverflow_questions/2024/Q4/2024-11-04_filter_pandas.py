6a7e599a5e9ad32ae5f6b19874e5752fd5036c19
Pieter Roggemans <pieter.roggemans@gmail.com>
2024-11-30 08:44:16 +0100

    Update 2024-11-04_filter_pandas.py

--- pysnippets/stackoverflow_questions/2024/Q4/2024-11-04_filter_pandas.py
+++ pysnippets/stackoverflow_questions/2024/Q4/2024-11-04_filter_pandas.py
@@ -4,6 +4,7 @@ https://stackoverflow.com/questions/79154874/combine-different-shapefile-feature
 
 import geopandas as gpd
 from shapely import box
+from shapely.validation import make_valid
 
 vec_data = gpd.read_file(r"C:\Temp\LAFIS\LAFIS_update.shp")
 """
@@ -13,12 +14,13 @@ vec_data = gpd.GeoDataFrame(
         "Meadow (Half Sheed Tara 20%)",
         "Meadow (permanent meadow)",
         "Pasture (tare 20%)/S28-6 Wooded pastures",
-        'Grain',
+        "Grain",
     ]},
     geometry=[box(0, 0, 5, 5), box(5, 0, 10, 5), box(10, 0, 15, 5), box(15, 0, 20, 5)],
     crs=31370,
 )
 """
+
 
 # Add new "Classes" column based on DESCR_ENG column
 vec_data["Classes"] = None
@@ -26,6 +28,11 @@ vec_data.loc[vec_data["DESCR_ENG"].str.startswith("Meadow"), "Classes"] = "Agric
 vec_data.loc[vec_data["DESCR_ENG"].str.startswith("Pasture"), "Classes"] = "Cultivated"
 
 vec_data_dissolved = vec_data.dissolve(by="Classes")
+
+vec_data_dissolved.geometry = vec_data_dissolved.apply(
+    lambda row: make_valid(row.geometry) if row.Classes == "Cultivated" else row.geometry,
+    axis=1,
+)
 
 # Print result
 print(vec_data.head())
