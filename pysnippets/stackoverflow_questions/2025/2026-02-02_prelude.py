378ba4d0f0bae47feea4dbf7a1d94038bbed62be
Pieter Roggemans <pieter.roggemans@gmail.com>
2025-02-06 09:13:11 +0100

    Update 2026-02-02_prelude.py

--- pysnippets/stackoverflow_questions/2025/2026-02-02_prelude.py
+++ pysnippets/stackoverflow_questions/2025/2026-02-02_prelude.py
@@ -6,9 +6,8 @@ import geopandas as gpd
 import os
 
 # SetDecimalPrecision function is not available in prelude_statements
-os.system(
-    r'ogr2ogr -dialect sqlite -oo PRELUDE_STATEMENTS="SELECT SetDecimalPrecision(1)" -sql "SELECT * FROM \"parcels\"" "C:\Temp\polygon_parcel\polygon-parcel2.gpkg" "C:\Temp\polygon_parcel\polygon-parcel.gpkg"'
-)
+cmd = '''ogr2ogr -dialect sqlite -oo PRELUDE_STATEMENTS="SELECT enable_load_extension(1);SELECT load_extension('mod_spatialite');SELECT SetDecimalPrecision(1)" -sql "SELECT * FROM \"parcels\"" "C:\Temp\polygon_parcel\polygon-parcel2.gpkg" "C:\Temp\polygon_parcel\polygon-parcel.gpkg"'''
+os.system(cmd)
 
 print(gpd.read_file("C:/Temp/polygon_parcel/polygon-parcel.gpkg"))
 print(gpd.read_file("C:/Temp/polygon_parcel/polygon-parcel2.gpkg"))
