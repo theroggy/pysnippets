825101d21625522bf86a37974708ba7952e84706
Pieter Roggemans <pieter.roggemans@gmail.com>
2025-04-14 09:29:51 +0200

    Create 2025-04-11_shp_real_warning.py

---
+++ pysnippets/pyogrio/issues/2025-04-11_shp_real_warning.py
@@ -0,0 +1,33 @@
+"""Read and write a shapefile using pyogrio leads to RuntimeWarnings."""
+
+from pathlib import Path
+
+import geodatasets
+import geopandas as gpd
+from geopandas.testing import assert_geodataframe_equal
+
+script_dir = Path(__file__).parent.resolve()
+
+path = geodatasets.get_path("nybb")
+
+print(f"Reading {path}")
+gdf = gpd.read_file(path)
+
+# Write to shapefile again
+for engine, use_arrow in [("fiona", None), ("pyogrio", False), ("pyogrio", True)]:
+    print("----------------------------------------------------")
+    print(f"Writing {path} using {engine=}, {use_arrow=}")
+    print("----------------------------------------------------")
+    output_path = script_dir / f"out_{engine}_{use_arrow}.shp"
+    if use_arrow is not None:
+        gdf.to_file(output_path, engine=engine, use_arrow=use_arrow)
+    else:
+        gdf.to_file(output_path, engine=engine)
+
+    # Warnings written:
+    # RuntimeWarning: Value 1623819823.8099999 of field Shape_Area of feature 0 not
+    # successfully written. Possibly due to too larger number with respect to field width
+    #  ogr_write(
+
+    read = gpd.read_file(output_path)
+    assert_geodataframe_equal(read, gdf)
