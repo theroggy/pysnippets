1ed648915f01066a8c0674b2fd92cbbd761dc9e2
Pieter Roggemans <pieter.roggemans@gmail.com>
2025-07-07 16:13:51 +0200

    Update 2025-07-04_distances.py

--- pysnippets/stackoverflow_questions/2025/2025-07-04_distances.py
+++ pysnippets/stackoverflow_questions/2025/2025-07-04_distances.py
@@ -3,14 +3,18 @@ import pandas as pd
 
 autobahn = "A7"
 columns = ["ref", "length_m"]
+expected_distance = 1924
+print(f"{autobahn}, expected distance: {expected_distance} km")
 
 df_total = pd.DataFrame(columns=columns)
 
 gdf = gpd.read_file(f"https://fliessbaden.de/wp-content/uploads/A7.geojson")
-gdf = gdf.to_crs("ESRI:102031")
 
-gdf["length_m"] = gdf.geometry.length
+for crs in ["ESRI:102031", "EPSG:25833", "EPSG:32632", "EPSG:4839"]:
+    gdf = gdf.to_crs(crs)
 
-total_m = gdf['length_m'].sum()
-total_km = total_m / 1000
-print(autobahn + ": " + str(round(total_km, 2)) + " km")
+    gdf["length_m"] = gdf.geometry.length
+
+    total_m = gdf['length_m'].sum()
+    total_km = total_m / 1000
+    print(f"{autobahn}, {crs} ({gdf.crs.name}): {str(round(total_km, 2))} km")
