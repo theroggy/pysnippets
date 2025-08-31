2e1391ef5997a50233a64d9e94a404a5031c4799
Pieter Roggemans <pieter.roggemans@gmail.com>
2025-08-31 16:33:26 +0200

    Update 2025-03-25_shp_zip_layername.py

--- pysnippets/gdal/2025-03-25_shp_zip_layername.py
+++ pysnippets/gdal/2025-03-25_shp_zip_layername.py
@@ -7,10 +7,12 @@ gdal.UseExceptions()
 
 gfo_uri = "https://github.com/geofileops/geofileops/raw/refs/heads/main"
 src_path = f"/vsicurl/{gfo_uri}/tests/data/polygon-parcel.gpkg"
-zip_path = f"{tempfile.gettempdir()}/parcels.shp.zip"
 
-file = gdal.VectorTranslate(destNameOrDestDS=zip_path, srcDS=src_path)
-file = None
-zip_file = gdal.OpenEx(zip_path, gdal.OF_VECTOR)
-zip_layer = zip_file.GetLayer(0)
-print(zip_layer.GetName())
+for dst_suffix in [".shp.zip", ".gpkg.zip"]:
+    zip_path = f"{tempfile.gettempdir()}/parcels{dst_suffix}"
+
+    file = gdal.VectorTranslate(destNameOrDestDS=zip_path, srcDS=src_path)
+    file = None
+    zip_file = gdal.OpenEx(zip_path, gdal.OF_VECTOR)
+    zip_layer = zip_file.GetLayer(0)
+    print(zip_layer.GetName())
