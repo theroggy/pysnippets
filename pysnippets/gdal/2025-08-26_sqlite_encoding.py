b95a6ee60a66860af1cc2d941157429007e5bae9
Pieter Roggemans <pieter.roggemans@gmail.com>
2025-08-31 16:33:22 +0200

    Update 2025-08-26_sqlite_encoding.py

--- pysnippets/gdal/2025-08-26_sqlite_encoding.py
+++ pysnippets/gdal/2025-08-26_sqlite_encoding.py
@@ -8,15 +8,24 @@ from osgeo import gdal
 gdal.UseExceptions()
 
 # Create a sqlite file
-script_path = Path(__file__)
-df = pd.DataFrame({"name": ["foö", "bÏr"], "value": [1, 2]})
-path = script_path.with_suffix(".sqlite")
-if path.exists():
-    path.unlink()
-con = sqlite3.connect(str(path))
-df.to_sql(name="test", index=True, con=con)
-con.close()
 
+create_new_file = False
+
+if create_new_file:
+    script_path = Path(__file__)
+    path = script_path.with_suffix(".sqlite")
+
+    df = pd.DataFrame({"name": ["foö", "bÏr"], "value": [1, 2]})
+
+    if path.exists():
+        path.unlink()
+    con = sqlite3.connect(str(path))
+    df.to_sql(name="test", index=True, con=con)
+    con.close()
+else:
+    path = Path(r"C:\Users\Gebruiker\Documents\GitHub\pyogrio\bla.db")
+
+# Open the datasource and print TestCapability of OLCStringsAsUTF8
 datasource = gdal.OpenEx(str(path))
 datasource_layer = datasource.GetLayer()
 
@@ -25,4 +34,13 @@ print(f"{datasource_layer.TestCapability(gdal.ogr.OLCStringsAsUTF8)=}")
 datasource_layer = None
 datasource = None
 
-pyogrio.read_dataframe(str(path))
+# Read and print the table
+print(pyogrio.read_dataframe(str(path)))
+
+# Determine encoding
+con = sqlite3.connect(str(path))
+cursor = con.cursor()
+cursor.execute("PRAGMA encoding;")
+encoding = cursor.fetchone()[0]
+print(f"Database encoding: {encoding}")
+con.close()
