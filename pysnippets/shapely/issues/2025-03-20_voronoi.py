25e27f509af4b436efaf7c50d88ad5b13886b024
Pieter Roggemans <pieter.roggemans@gmail.com>
2026-01-09 19:30:40 +0100

    Update 2025-03-20_voronoi.py

--- pysnippets/shapely/issues/2025-03-20_voronoi.py
+++ pysnippets/shapely/issues/2025-03-20_voronoi.py
@@ -3,23 +3,30 @@ import shapely
 import shapely.plotting as plotter
 
 centers = [
-   [(366.99763488064747, -45.610000000000014)],
-   [(366.2381975042589, -45.339682883479995)],
-   [(366.0603171165201, -45.161802495741185)],
-   [(365.7900000000001, -44.40236511935221)]
+   (366.99763488064747, -45.610000000000014),
+   (366.2381975042589, -45.339682883479995),
+   (366.0603171165201, -45.161802495741185),
+   (365.7900000000001, -44.40236511935221)
 ]
 
-polygons = shapely.voronoi_polygons(shapely.MultiPoint(centers))
-print(len(polygons.geoms))
-fig, axes = plt.subplots(ncols=len(polygons.geoms) + 1, sharex=True, sharey=True)
-for idx, polygon in enumerate(polygons.geoms):
-    plotter.plot_polygon(polygon, axes[idx])
-for ax in axes:
-    ax.set_aspect('equal', adjustable='box')
-    for center in centers:
-        plotter.plot_points(shapely.Point(center[0]), color="red", ax=ax)
+fig, axes = plt.subplots(ncols=len(centers) + 1, nrows=2)
 
-for polygon in polygons.geoms:
-    plotter.plot_polygon(polygon, axes[-1])
+for factor_idx, factor in enumerate([1, 10]):
+    scaled_centers = [[(x * factor, y * factor)] for (x, y) in centers]
+    points = shapely.MultiPoint(scaled_centers)
+    print(f"Points: {points}")
+    polygons = shapely.voronoi_polygons(points)
+    print(f"Scale factor used: {factor}, Number of polygons: {len(polygons.geoms)}")
+
+    for idx, polygon in enumerate(polygons.geoms):
+        plotter.plot_polygon(polygons.envelope, axes[factor_idx][idx], color="lightgray", alpha=0.5)
+        plotter.plot_polygon(polygon, axes[factor_idx][idx])
+    for ax in axes[factor_idx]:
+        ax.set_aspect('equal')
+        for center in scaled_centers:
+            plotter.plot_points(shapely.Point(center[0]), color="red", ax=ax)
+
+    for polygon in polygons.geoms:
+        plotter.plot_polygon(polygon, axes[factor_idx][-1])
 
 plt.show()
