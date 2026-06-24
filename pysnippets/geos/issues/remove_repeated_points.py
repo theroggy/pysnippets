409a93fc6a9fc0cc305ee30d96d73568869f9542
Pieter Roggemans <pieter.roggemans@gmail.com>
2026-06-25 01:37:53 +0200

    Update remove_repeated_points.py

--- pysnippets/geos/issues/remove_repeated_points.py
+++ pysnippets/geos/issues/remove_repeated_points.py
@@ -2,7 +2,9 @@
 
 - set_precision: repeated points are removed.
 - is_valid: repeated points are not considered as making a polygon invalid.
-- make_valid: repeated points are NOT removed.
+- make_valid:
+   - if the input polygon is valid, repeated points are NOT removed.
+   - if the input polygon is invalid, repeated points are removed.
 """
 
 import shapely
@@ -31,11 +33,28 @@ print(f"{count_coordinates(poly_no_dups)=}")
 print(f"{count_coordinates(rounded)=}")
 print(f"{count_coordinates(rounded_no_dups)=}")
 
-# Behaviour of make_valid
-print("Test make_valid with a polygon with repeated points")
+# Behaviour of make_valid on a valid polygon with repeated points
+print("Test make_valid with a valid polygon with repeated points")
 poly_wkt = "POLYGON ((0 0, 1 0, 1 0, 1 1, 0 1, 0 0.9, 0 0))"
 poly = shapely.from_wkt(poly_wkt)
 assert poly.is_valid
 poly_valid = shapely.make_valid(poly)
+
 print(f"{count_coordinates(poly)=}")
 print(f"{count_coordinates(poly_valid)=}")
+
+# Behavior of make_valid on an invalid polygon with repeated points
+print("Test make_valid with an invalid polygon with repeated points")
+poly_wkt = (
+    "POLYGON ((0 10, 10 0, 10 0, 10 10, 0 0, 0 10), (5 5, 5 5, 5 5, 5 5))"
+)
+poly = shapely.from_wkt(poly_wkt)
+assert not poly.is_valid
+poly_valid = shapely.make_valid(poly)
+assert poly_valid.is_valid
+poly_valid_no_dups = shapely.remove_repeated_points(poly_valid)
+
+print(f"{count_coordinates(poly)=}")
+print(f"{count_coordinates(poly_valid)=}")
+print(f"{poly_valid.wkt=}")
+print(f"{count_coordinates(poly_valid_no_dups)=}")
