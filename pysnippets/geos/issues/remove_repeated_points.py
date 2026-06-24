10778bace71f00c1464f3668a92f6aea76c4c783
Pieter Roggemans <pieter.roggemans@gmail.com>
2026-06-25 00:33:12 +0200

    Update remove_repeated_points.py

--- pysnippets/geos/issues/remove_repeated_points.py
+++ pysnippets/geos/issues/remove_repeated_points.py
@@ -1,7 +1,41 @@
-import shapely
+"""Script to check how repeated points are treated by several functions.
 
-poly_wkt = "Polygon ((139770.26822331376024522 188334.00010800323798321, 139769.5 188338.01162790699163452, 139769.5 188338.3723930635896977, 139769.5 188338.5, 139769.81343283582828008 188338.5, 139770.375 188339.375, 139772.39924806414637715 188340.26989983080420643, 139770.26822331376024522 188334.00010800323798321),(139769.75256541155977175 188338.40516005983226933, 139769.75256541153066792 188338.40516005983226933, 139769.75256541153066792 188338.4051600598031655, 139769.75256541155977175 188338.40516005983226933))"  # noqa: E501
+- set_precision: repeated points are removed.
+- is_valid: repeated points are not considered as making a polygon invalid.
+- make_valid: repeated points are NOT removed.
+"""
+
+import shapely
+from shapely import count_coordinates
+
+
+# Behaviour of set_precision and remove_repeated_points towards (nearly) repeated points
+print("Test set_precision with a polygon with (nearly)repeated points")
+poly = shapely.Polygon([(0, 0), (0.9, 0), (1, 0), (1, 1), (0, 1), (0, 0.9), (0, 0)])
+poly_no_dups = shapely.remove_repeated_points(poly)
+rounded = shapely.set_precision(poly, grid_size=1)
+rounded_no_dups = shapely.remove_repeated_points(rounded)
+
+print(f"{count_coordinates(poly)=}")
+print(f"{count_coordinates(poly_no_dups)=}")
+print(f"{count_coordinates(rounded)=}")
+print(f"{count_coordinates(rounded_no_dups)=}")
+
+poly = shapely.Polygon([(0, 0), (1, 0), (1, 0), (1, 1), (0, 1), (0, 0.9), (0, 0)])
+poly_no_dups = shapely.remove_repeated_points(poly)
+rounded = shapely.set_precision(poly, grid_size=1)
+rounded_no_dups = shapely.remove_repeated_points(rounded)
+
+print(f"{count_coordinates(poly)=}")
+print(f"{count_coordinates(poly_no_dups)=}")
+print(f"{count_coordinates(rounded)=}")
+print(f"{count_coordinates(rounded_no_dups)=}")
+
+# Behaviour of make_valid
+print("Test make_valid with a polygon with repeated points")
+poly_wkt = "POLYGON ((0 0, 1 0, 1 0, 1 1, 0 1, 0 0.9, 0 0))"
 poly = shapely.from_wkt(poly_wkt)
 assert poly.is_valid
-
-shapely.remove_repeated_points(poly, tolerance=1e-8)
+poly_valid = shapely.make_valid(poly)
+print(f"{count_coordinates(poly)=}")
+print(f"{count_coordinates(poly_valid)=}")
